17ad01b35a76b9ac6cc2daea3f3a5a3a02f96f30
JesusSePe <umbreon20019@gmail.com>
2021-10-13 20:18:02 +0200

    first commit

---
+++ app/src/main/java/com/example/mylist/MainActivity.java
@@ -0,0 +1,80 @@
+package com.example.mylist;
+
+import android.os.Bundle;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.*;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import java.util.ArrayList;
+
+/*
+    Abans de començar hem de crear
+    (1) Un layout per al main amb una ListView anomenada recordsView
+            R.id.recordsView
+    (2) Un layout (lineal) per a la ListView anomenat list_item
+            R.layout.list_item
+        Aquest contindrà 2 TextView amb IDs: nom i intents
+            R.id.nom
+            R.id.intents
+
+    Referències
+        - https://github.com/codepath/android_guides/wiki/Using-an-ArrayAdapter-with-ListView
+ */
+
+public class MainActivity extends AppCompatActivity {
+
+    // Model: Record (intents=puntuació, nom)
+    class Record {
+        public int intents;
+        public String nom;
+
+        public Record(int _intents, String _nom ) {
+            intents = _intents;
+            nom = _nom;
+        }
+    }
+    // Model = Taula de records: utilitzem ArrayList
+    ArrayList<Record> records;
+
+    // ArrayAdapter serà l'intermediari amb la ListView
+    ArrayAdapter<Record> adapter;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        // Inicialitzem model
+        records = new ArrayList<Record>();
+        // Afegim alguns exemples
+        records.add( new Record(33,"Manolo") );
+        records.add( new Record(12,"Pepe") );
+        records.add( new Record(42,"Laura") );
+
+        // Inicialitzem l'ArrayAdapter amb el layout pertinent
+        adapter = new ArrayAdapter<Record>( this, R.layout.list_item, records )
+        {
+            @Override
+            public View getView(int pos, View convertView, ViewGroup container)
+            {
+                // getView ens construeix el layout i hi "pinta" els valors de l'element en la posició pos
+                if( convertView==null ) {
+                    // inicialitzem l'element la View amb el seu layout
+                    convertView = getLayoutInflater().inflate(R.layout.list_item, container, false);
+                }
+                // "Pintem" valors (també quan es refresca)
+                ((TextView) convertView.findViewById(R.id.nom)).setText(getItem(pos).nom);
+                ((TextView) convertView.findViewById(R.id.intents)).setText(Integer.toString(getItem(pos).intents));
+                return convertView;
+            }
+
+        };
+
+        // busquem la ListView i li endollem el ArrayAdapter
+        ListView lv = (ListView) findViewById(R.id.recordsView);
+        lv.setAdapter(adapter);
+    }
+
+}
